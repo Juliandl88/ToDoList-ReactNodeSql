00a5d16b885268949ef4ba5eee999156c9b638ef
Julián De Lisio <76756541+Juliandl88@users.noreply.github.com>
2021-12-30 23:51:35 -0300

    actualizacion

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Nooo no me copio la app
         </p>
         <a
           className="App-link"
